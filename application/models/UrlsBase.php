92a02317ecc2d073f11636e1ce5c6fc608ca39e6
a.kuzmin <anton.kuzmin@marketgid.com>
2016-09-17 09:37:48 +0300

    copyrights & comments

--- application/models/UrlsBase.php
+++ application/models/UrlsBase.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @package WebScout
+ * @see for EN http://hack4sec.pro/wiki/index.php/WebScout_en
+ * @see for RU http://hack4sec.pro/wiki/index.php/WebScout
+ * @license MIT
+ * @copyright (c) Anton Kuzmin <http://anton-kuzmin.ru> (ru) <http://anton-kuzmin.pro> (en)
+ * @author Anton Kuzmin
+ */
 class UrlsBase extends Zend_Db_Table_Abstract {
     protected $_name = 'urls_base';
 
